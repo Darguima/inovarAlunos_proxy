f68430f474c879e0b6a052ef81b53a000f2a499b
Darguima <dsgdevbraga@gmail.com>
2022-02-12 16:09:32 +0000

    Corrections for Typescript

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import { createProxyMiddleware } from 'http-proxy-middleware'
 import modifyResponse from 'node-http-proxy-json'
 
+import { inovarApiResponse } from './types/inovarApiResponse'
+
 const app = express()
 
 app.use(
@@ -11,23 +13,23 @@ app.use(
       target: 'https://193.236.33.154',
       headers: { Host: 'alunos.aecarlosamarante.pt' },
       changeOrigin: true,
-      onProxyRes
+
+      onProxyRes: (proxyRes, req, res) => {
+        console.log('proxy requested')
+
+        switch (req.url) {
+          case '/api/faltas/44849/1':
+            modifyResponse(res, proxyRes.headers['content-encoding'], (body: inovarApiResponse.faltas) => {
+              if (body) {
+                body.Faltas = [body.Faltas[1]]
+              }
+              return body
+            })
+            break
+        }
+      }
     }
   )
 )
-
-function onProxyRes (proxyRes, req, res) {
-  console.log('proxy requested')
-  if (req._parsedUrl.pathname == '/api/faltas/44849/1') {
-    delete proxyRes.headers['content-length']
-
-    modifyResponse(res, proxyRes.headers['content-encoding'], (body) => {
-      if (body) {
-        body.Faltas = [body.Faltas[1]]
-      }
-      return body
-    })
-  }
-}
 
 app.listen(80)
---
+++ src/types/inovarApiResponse.d.ts
@@ -0,0 +1,34 @@
+/* eslint-disable no-tabs */
+
+export declare namespace inovarApiResponse {
+	export interface faltas {
+		NextRequest: string,
+		Faltas: Array<{
+			DataFalta: string,
+			DiaDaSemana: string,
+			Hora: string,
+			Disciplina: string,
+			Tipo: string,
+			TipoId: number,
+			Modulo: null,
+			TemModulo: boolean,
+			DataDescricao: string,
+			FI: number,
+			FJ: number,
+			FM: number,
+			FD: number,
+			FP: number,
+			TPC: number,
+			RI: number,
+			RJ: number,
+			MI: number,
+			MX: number,
+			Selected: boolean,
+			IdDisciplina: number,
+			HInicio: null,
+			HFim: null,
+			StatusPedido: number,
+			Justificacao: null
+		}>
+	}
+}
